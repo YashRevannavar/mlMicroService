fb471282627147ae081f23e774f69bb61010d525
yash revannavar <yashrevannavar13@gmail.com>
2023-04-22 13:22:05 +0200

    added tailwindcss

--- algorithm.py
+++ algorithm.py
@@ -8,8 +8,17 @@ def open_file():
         model = pickle.load(f)
     return model
 
+def validate(data):
+    if isinstance(data,float):
+        return data
+    else:
+        data = [[0,0,0,0]]
+        return data
+
+
 def predict_fucn():
     data = get_data_from_form()
+    data = validate(data=data)
     model = open_file()
     prediction = model.predict(data)
     print(f"no: {prediction}")
